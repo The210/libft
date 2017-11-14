f76b3a27086c423b9b53b519f283b599fc85e5cb
Daniel HORVILLEUR <dhorvill@e1r3p6.42.fr>
2017-11-14 17:34:38 +0100

    All green

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,13 +6,13 @@
 /*   By: dhorvill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 22:36:51 by dhorvill          #+#    #+#             */
-/*   Updated: 2017/11/14 16:17:41 by dhorvill         ###   ########.fr       */
+/*   Updated: 2017/11/14 17:30:28 by dhorvill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		ft_nb_words(char *s, char c)
+static int	nbw(char *s, char c)
 {
 	int i;
 	int sym;
@@ -27,12 +27,12 @@ static int		ft_nb_words(char *s, char c)
 	{
 		while (s[i] != c && s[i])
 		{
-			i++;
 			sym = 1;
+			i++;
 		}
-		if (s[i] == c)
+		while (s[i] == c && s[i])
 		{
-			count += (sym == 1) ? 1 : 0;
+			count += sym;
 			sym = 0;
 			i++;
 		}
@@ -40,7 +40,7 @@ static int		ft_nb_words(char *s, char c)
 	return (count + 2);
 }
 
-static int		lnw(char *s, int i, char c)
+static int	nbc(char *s, int i, char c)
 {
 	int count;
 
@@ -53,7 +53,7 @@ static int		lnw(char *s, int i, char c)
 	return (count + 1);
 }
 
-char			**ft_strsplit(const char *s, char c)
+char		**ft_strsplit(const char *s, char c)
 {
 	char	**res;
 	int		i;
@@ -62,25 +62,20 @@ char			**ft_strsplit(const char *s, char c)
 
 	i = 0;
 	j = 0;
-	if ((res = malloc(sizeof(char*) * (ft_nb_words((char*)s, c)))) == NULL)
+	s = ft_strtrimc(s, c);
+	if (!s || !(res = malloc(sizeof(char*) * nbw((char*)s, c))))
 		return (NULL);
-	if (s != NULL)
+	while (s[i])
 	{
-		while (s[i])
-		{
-			while (s[i] == c && s[i])
-				i++;
-			if (s[i])
-			{
-				k = 0;
-				if ((res[j] = malloc(sizeof(c) * lnw((char*)s, i, c))) == NULL)
-					return (NULL);
-				while (s[i] != c && s[i])
-					res[j][k++] = s[i++];
-				res[j++][k] = '\0';
-			}
-		}
-		res[j] = NULL;
+		k = 0;
+		if (!(res[j] = malloc(sizeof(c) * nbc((char*)s, i, c))))
+			return (NULL);
+		while (s[i] != c && s[i])
+			res[j][k++] = s[i++];
+		while (s[i] == c && s[i])
+			i++;
+		res[j++][k] = '\0';
 	}
+	res[j] = NULL;
 	return (res);
 }
